7f62e1b0a661760d689fb93c13a44b26c6b5c27c
maky99 <makyade99@gmail.com>
2023-09-25 22:39:40 -0300

    bien

--- Actividades/actividades.js
+++ Actividades/actividades.js
@@ -9,17 +9,18 @@
 //     }
 //     num = parseInt(prompt("Ingresando 10 se sale "))
 // }
-
+//=======================================================================
 // let num = parseInt(prompt("Ingresar un numero"));
-// alert("tu numero " + num + " se sumara 10 veces")
+// let num2 = parseInt(prompt("¿cuantas veces queres que sume tu numero : " + num + " ?"));
+// alert("tu numero " + num + " se sumara " + num2 + " veces")
 
 // let total = num;
-// for (let i = 0; i < 10; i++) {
+// for (let i = 0; i < num2; i++) {
 //     total = num + total;
 //     console.log(total - num, " + ", num, " = ", total)
 // }
 
-
+//=========================================================================
 // let texto = prompt("Ingrese esc para salir");
 // let clave = "esc";
 // let armo = " ";
@@ -31,11 +32,11 @@
 //     console.log("datos ingresados : ", armo);
 //     texto = prompt("Ingrese esc para salir");
 //     texto = texto.toLocaleLowerCase();
-
-
 // }
 // console.log("lo que ingresaste es: ", armo)
 
+
+//==============================================================================
 // let cant = prompt("ingresar un numero");
 // for (let i = 0; i < cant; i++) {
 //     console.log(i + 1, "Hola");
